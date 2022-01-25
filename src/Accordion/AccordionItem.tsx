8549c13d0ffd2b5709a05eb19fd8335bf1021a5b
Spasmochi <spasmochi@gmail.com>
2022-01-25 21:24:17 +1100

    more work

--- src/Accordion/AccordionItem.tsx
+++ src/Accordion/AccordionItem.tsx
@@ -1,16 +1,51 @@
-import { FC } from "react";
+import { createContext, FC, useMemo, useState, useCallback } from 'react'
 
 /*
  * Contains each item in the Accordion
  * Provides context to the children of the item
  */
 
-export type AccordionItemProps = {
-  id: number; // the item id (either passed or set by Accordion parent with index)
-  disabled?: boolean; // if true item is not clickable, false by default
-  expanded?: boolean; // if true body is expanded, false by default
-};
+const stub = (): never => {
+  throw new Error('You forgot to wrap your component in <CognitoProvider>.')
+}
 
-export const AccordionItem: FC<AccordionItemProps> = (props) => {
-  return <div></div>;
-};
+export type AccordionItemProps = {
+  id: string // the item id (either passed or set by Accordion parent with index)
+  disabled?: boolean // if true item is not clickable, false by default
+  expanded?: boolean // if true body is expanded, false by default
+}
+
+export const AccordionItem: FC<AccordionItemProps> = (props, children) => {
+  const { disabled, expanded } = props
+
+  const ctx = useMemo(() => ({ disabled, expanded }), [disabled, expanded])
+
+  const AccordionItem = createContext({
+    disabled,
+    expanded,
+    toggle: stub,
+  })
+
+  const toggle = useCallback(() => {
+  
+  }, []);
+
+  return (
+    <AccordionItem.Provider
+      value={{
+        ...ctx,
+        toggle
+      }}
+    >
+      <div id={props.id}>{children}</div>
+    </AccordionItem.Provider>
+  )
+}
+
+
+
+// <AI expanded={true} />
+
+// ai = useAI();
+// ai.toggle();
+
--- src/Accordion/useAccordion.ts
+++ src/Accordion/useAccordion.ts
@@ -1,13 +1,13 @@
-import { createContext, useState } from "react";
-import { useUnmountEffect } from "../hooks/useUnmountEffect";
+import { createContext, useCallback, useEffect, useRef, useState } from 'react'
+import { useUnmountEffect } from '../hooks/useUnmountEffect'
 
-type ExpandedIndex = number | Array<number>;
+type ExpandedIndex = number | Array<number>
 
 export interface AccordionProps {
-  allowMultiple?: boolean; // if true multiple items can be expanded and hence it allows toggle, false by default
-  expanded?: ExpandedIndex; // if true all are expanded, false by default
-  expandedIndex?: ExpandedIndex; // the id or ids of AccordionItems that are currently active
-  onChange?: () => void; // The callback invoked when accordion items are expanded or collapsed.
+  allowMultiple?: boolean // if true multiple items can be expanded and hence it allows toggle, false by default
+  // expanded?: boolean // if true all are expanded, false by default
+  expandedIndex?: ExpandedIndex // the id or ids of AccordionItems that are currently active
+  onChange?: () => void // The callback invoked when accordion items are expanded or collapsed.
 }
 
 /**
@@ -15,43 +15,55 @@ export interface AccordionProps {
  * for accordion items.
  */
 export function useAccordion(props: AccordionProps) {
-  const { allowMultiple, expanded, expandedIndex, onChange } = props;
+  const { allowMultiple, expandedIndex, onChange } = props
   //TODO add in some logic to validate props
 
   /**
    * This state manages the index of the focussed accordion toggle
    */
-  const [focusedIndex, setFocusedIndex] = useState(-1);
+  const [focusedIndex, setFocusedIndex] = useState(-1)
 
   /**
    * Reset focused index when accordion unmounts
    * or descendants change
    */
   useUnmountEffect(() => {
-    setFocusedIndex(-1);
-  });
+    setFocusedIndex(-1)
+  })
 
   /**
    * Hook that manages the controlled and un-controlled state
    * for the accordion.
    */
+  const [state, setState] = useState<AccordionProps>(props)
 
   return {
     focusedIndex,
-    setFocusedIndex
-  };
+    setFocusedIndex,
+  }
 }
 
+const selected = useRef(Item)
+
+const updateExpandedIndex = useCallback(
+  (item: ExpandedIndex) => {
+    selected.current = item
+    setFocusedIndex(item.index)
+  },
+  [setFocusedIndex]
+)
+
+
 export const AccordionContext = createContext({
-  name: "AccordionContext",
+  name: 'AccordionContext',
   errorMessage:
-    "useAccordionContext: `context` is undefined. Seems you forgot to wrap the accordion components in `<Accordion />`"
-});
+    'useAccordionContext: `context` is undefined. Seems you forgot to wrap the accordion components in `<Accordion />`',
+})
 
 export interface AccordionItemProps {
-  id: number; // the item id (either passed or set by Accordion parent with index)
-  isDisabled?: boolean; // if true item is not clickable, false by default
-  expanded?: boolean; // if true body is expanded, false by default
+  id: number // the item id (either passed or set by Accordion parent with index)
+  isDisabled?: boolean // if true item is not clickable, false by default
+  expanded?: boolean // if true body is expanded, false by default
 }
 
 ////? Do I need a descendant context?
